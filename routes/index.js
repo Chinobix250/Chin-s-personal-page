6681bf363e6601c6e0ea4b98a5a02f3ca6fef897
Chinobix250 <140477511+Chinobix250@users.noreply.github.com>
2025-11-19 19:03:49 -0300

    Commit 2
    Commit de de todos los archivos de página.

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+router.get("/music_scores", (req, res) => {
+    res.render("music_scores")
+})
+
+router.get("/programming", (req, res) => {
+    res.render("programming")
+})
+
+router.get("/misc", (req, res) => {
+    res.render("misc")
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+const path = require("path")
+const routerIndex = require("./routes/index")
+const morgan = require("morgan")
+
+app.listen(port, () => { // Arranca el servidor.
+    console.log(`Servidor corriendo en http://localhost:${port}.`)
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan("dev"))
+
+// Configurar EJS como motor de plantillas.
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+// Configurar Express para servir páginas estáticas.
+//app.use(express.static("public"))
+
+app.use("/", routerIndex) // Usar rutas.
+app.use((req, res) => { // Error 404.
+    res.status(404).render("404")
+})
+app.use(express.json()) // Para manejar JSON.
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
